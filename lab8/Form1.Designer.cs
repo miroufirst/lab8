613e8c4de2553c93cf3760e7b4cbce90edf3decd
Адмін <@DESKTOP-RQHPOUK>
2025-12-30 03:05:18 +0200

    Add project files.

---
+++ lab8/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSedan = new System.Windows.Forms.Button();
+            this.btnSuv = new System.Windows.Forms.Button();
+            this.btnGps = new System.Windows.Forms.Button();
+            this.btnLeather = new System.Windows.Forms.Button();
+            this.btnSpoiler = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSedan
+            // 
+            this.btnSedan.Location = new System.Drawing.Point(12, 12);
+            this.btnSedan.Name = "btnSedan";
+            this.btnSedan.Size = new System.Drawing.Size(189, 23);
+            this.btnSedan.TabIndex = 0;
+            this.btnSedan.Text = "New Sedan (10 000$)";
+            this.btnSedan.UseVisualStyleBackColor = true;
+            // 
+            // btnSuv
+            // 
+            this.btnSuv.Location = new System.Drawing.Point(12, 41);
+            this.btnSuv.Name = "btnSuv";
+            this.btnSuv.Size = new System.Drawing.Size(189, 23);
+            this.btnSuv.TabIndex = 1;
+            this.btnSuv.Text = "New SUV (15 000$)";
+            this.btnSuv.UseVisualStyleBackColor = true;
+            // 
+            // btnGps
+            // 
+            this.btnGps.Location = new System.Drawing.Point(12, 70);
+            this.btnGps.Name = "btnGps";
+            this.btnGps.Size = new System.Drawing.Size(189, 23);
+            this.btnGps.TabIndex = 2;
+            this.btnGps.Text = "Add GPS (+500$)";
+            this.btnGps.UseVisualStyleBackColor = true;
+            // 
+            // btnLeather
+            // 
+            this.btnLeather.Location = new System.Drawing.Point(12, 99);
+            this.btnLeather.Name = "btnLeather";
+            this.btnLeather.Size = new System.Drawing.Size(189, 23);
+            this.btnLeather.TabIndex = 3;
+            this.btnLeather.Text = "Add Leather (+1000$)";
+            this.btnLeather.UseVisualStyleBackColor = true;
+            // 
+            // btnSpoiler
+            // 
+            this.btnSpoiler.Location = new System.Drawing.Point(12, 128);
+            this.btnSpoiler.Name = "btnSpoiler";
+            this.btnSpoiler.Size = new System.Drawing.Size(189, 23);
+            this.btnSpoiler.TabIndex = 4;
+            this.btnSpoiler.Text = "Add Spoiler (+300$)";
+            this.btnSpoiler.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(23, 166);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(87, 16);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Select a car...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 529);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSpoiler);
+            this.Controls.Add(this.btnLeather);
+            this.Controls.Add(this.btnGps);
+            this.Controls.Add(this.btnSuv);
+            this.Controls.Add(this.btnSedan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSedan;
+        private System.Windows.Forms.Button btnSuv;
+        private System.Windows.Forms.Button btnGps;
+        private System.Windows.Forms.Button btnLeather;
+        private System.Windows.Forms.Button btnSpoiler;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ lab8/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        private ICar _currentCar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AttachEvents();
+        }
+
+        private void AttachEvents()
+        {
+            btnSedan.Click += (s, e) => CreateCar(new Sedan());
+            btnSuv.Click += (s, e) => CreateCar(new SUV());
+
+            btnGps.Click += (s, e) => AddDecorator(new GPS(_currentCar));
+            btnLeather.Click += (s, e) => AddDecorator(new LeatherSeats(_currentCar));
+            btnSpoiler.Click += (s, e) => AddDecorator(new Spoiler(_currentCar));
+        }
+
+        private void CreateCar(ICar car)
+        {
+            _currentCar = car;
+            UpdateDisplay();
+        }
+
+        private void AddDecorator(ICar decorator)
+        {
+            if (_currentCar == null)
+            {
+                MessageBox.Show("Please select a base car first!");
+                return;
+            }
+
+            _currentCar = decorator;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            if (_currentCar != null)
+            {
+                lblResult.Text = $"Description: {_currentCar.GetDescription()}\n\n" +
+                                 $"Total Cost: {_currentCar.GetCost()} $";
+            }
+        }
+    }
+
+    public interface ICar
+    {
+        string GetDescription();
+        double GetCost();
+    }
+
+    public class Sedan : ICar
+    {
+        public string GetDescription() => "Sedan";
+        public double GetCost() => 10000;
+    }
+
+    public class SUV : ICar
+    {
+        public string GetDescription() => "SUV";
+        public double GetCost() => 15000;
+    }
+
+    public abstract class CarDecorator : ICar
+    {
+        protected ICar _car;
+
+        public CarDecorator(ICar car)
+        {
+            _car = car;
+        }
+
+        public virtual string GetDescription() => _car.GetDescription();
+        public virtual double GetCost() => _car.GetCost();
+    }
+
+    public class GPS : CarDecorator
+    {
+        public GPS(ICar car) : base(car) { }
+
+        public override string GetDescription() => _car.GetDescription() + ", GPS Navigation";
+        public override double GetCost() => _car.GetCost() + 500;
+    }
+
+    public class LeatherSeats : CarDecorator
+    {
+        public LeatherSeats(ICar car) : base(car) { }
+
+        public override string GetDescription() => _car.GetDescription() + ", Leather Interior";
+        public override double GetCost() => _car.GetCost() + 1000;
+    }
+
+    public class Spoiler : CarDecorator
+    {
+        public Spoiler(ICar car) : base(car) { }
+
+        public override string GetDescription() => _car.GetDescription() + ", Sport Spoiler";
+        public override double GetCost() => _car.GetCost() + 300;
+    }
+}
